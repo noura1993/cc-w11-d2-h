dbbcaa25ad77e9055ee2895178f93b83b5d8889b
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-21 21:09:38 +0100

    Add test for countBooks

--- Library/src/main/java/Library.java
+++ Library/src/main/java/Library.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<Book>();
+    }
+
+    public int bookCount() {
+        return this.books.size();
+    }
 }
--- Library/src/test/java/LibraryTest.java
+++ Library/src/test/java/LibraryTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+    @Before
+    public void before() {
+        library = new Library();
+        book1 = new Book("Real Life", "Brandon Taylor", "Novel");
+        book2 = new Book("The Glass Hotel", "Emily St.John", "Novel");
+        book3 = new Book("Harry Potter", "J.K. Rowling", "Novel");
+    }
+
+    @Test
+    public void countBooks() {
+        assertEquals(0, library.bookCount());
+    }
+
+
 }
