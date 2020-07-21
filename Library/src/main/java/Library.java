dd27190cc54859bd2d6e436df3e57a9ade908d67
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-21 21:28:37 +0100

    Add test to check capacity

--- Library/src/main/java/Library.java
+++ Library/src/main/java/Library.java
@@ -2,13 +2,23 @@ import java.util.ArrayList;
 
 public class Library {
 
+    private int capacity;
     private ArrayList<Book> books;
 
-    public Library() {
+    public Library(int capacity) {
+        this.capacity = capacity;
         this.books = new ArrayList<Book>();
     }
 
     public int bookCount() {
         return this.books.size();
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
+
+    public boolean checkCapacity() {
+        return this.capacity > this.bookCount();
     }
 }
--- Library/src/test/java/LibraryTest.java
+++ Library/src/test/java/LibraryTest.java
@@ -1,7 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class LibraryTest {
     private Library library;
@@ -11,7 +11,7 @@ public class LibraryTest {
 
     @Before
     public void before() {
-        library = new Library();
+        library = new Library(2);
         book1 = new Book("Real Life", "Brandon Taylor", "Novel");
         book2 = new Book("The Glass Hotel", "Emily St.John", "Novel");
         book3 = new Book("Harry Potter", "J.K. Rowling", "Novel");
@@ -20,6 +20,26 @@ public class LibraryTest {
     @Test
     public void countBooks() {
         assertEquals(0, library.bookCount());
+    }
+
+    @Test
+    public void addBook() {
+        library.addBook(book1);
+        library.addBook(book2);
+        assertEquals(2, library.bookCount());
+    }
+
+    @Test
+    public void canAddBook() {
+        library.addBook(book1);
+        assertTrue(library.checkCapacity());
+    }
+
+    @Test
+    public void cannotAddBook() {
+        library.addBook(book1);
+        library.addBook(book2);
+        assertFalse(library.checkCapacity());
     }
 
 
